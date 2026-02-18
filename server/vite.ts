d352d90fc3c0732f9d0137928d2f0b1aaf1d9605
perfkote <Scott@performancekote.com>
2026-02-17 21:46:58 -0500

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -80,6 +80,7 @@ export function serveStatic(app: Express) {
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
+    res.setHeader("Content-Type", "text/html");
     res.sendFile(path.resolve(distPath, "index.html"));
-  });
+});
 }
